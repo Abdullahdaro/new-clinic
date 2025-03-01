e05210bfac2a47fa4ee83fce3271ee9a55ec2476
AbdullahDaro <abode.morx@gmail.com>
2025-03-02 02:44:37 +0800

    fix

--- src/pages/treatments/aesthetic-gum.js
+++ src/pages/treatments/aesthetic-gum.js
@@ -69,14 +69,14 @@ export default function AestheticGum() {
             
             <h2 className="text-2xl font-semibold text-gray-900">Benefits</h2>
             <ul className="space-y-2">
-              {pageContent.benefits.map((benefit, index) => (
+{/*               {pageContent.benefits.map((benefit, index) => (
                 <li key={index} className="flex items-center gap-2 text-gray-600">
                   <svg className="w-5 h-5 text-main" fill="currentColor" viewBox="0 0 20 20">
                     <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                   </svg>
                   {benefit}
                 </li>
-              ))}
+              ))} */}
             </ul>
           </div>
 
